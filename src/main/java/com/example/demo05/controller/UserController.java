0c1b71da4560c2fe09f2bd76cd8b0b6410758bc2
1774214410 <1774214410@qq.com>
2018-04-13 23:43:32 +0800

    springboot加持lombok，方便开发

--- src/main/java/com/example/demo05/controller/UserController.java
+++ src/main/java/com/example/demo05/controller/UserController.java
@@ -6,6 +6,7 @@ import com.example.demo05.utils.JsonResult;
 import com.example.demo05.utils.ResultCode;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import javax.annotation.Resource;
 import java.util.List;
 @RestController
 @RequestMapping(value = "/user")
+@Log4j
 public class UserController {
 
     @Resource
@@ -24,8 +26,12 @@ public class UserController {
     @RequestMapping(value = "/findAll")
     public JsonResult findAll(@RequestParam(name = "pageNum",defaultValue = "1",required = false) int pageNum,
                               @RequestParam(name = "pageSize",defaultValue = "2",required = false) int pageSize){
-        PageHelper.startPage(pageNum,pageSize);       //分页核心代码   就这一句
+        PageHelper.startPage(pageNum,pageSize);
+
+        log.//分页核心代码   就这一句
         List<User> userList = userMapper.findAll();
+        User user = new User();
+
         PageInfo<User> pageInfo = new PageInfo<>(userList);
         return new JsonResult(ResultCode.SUCCESS,pageInfo);
     }
--- src/main/java/com/example/demo05/model/User.java
+++ src/main/java/com/example/demo05/model/User.java
@@ -1,6 +1,8 @@
 package com.example.demo05.model;
 
+import lombok.Data;
 
+@Data
 public class User {
 
     private int id;
@@ -12,44 +14,4 @@ public class User {
     private String user_phone;
 
     private String user_addr;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUser_name() {
-        return user_name;
-    }
-
-    public void setUser_name(String user_name) {
-        this.user_name = user_name;
-    }
-
-    public int getUser_age() {
-        return user_age;
-    }
-
-    public void setUser_age(int user_age) {
-        this.user_age = user_age;
-    }
-
-    public String getUser_phone() {
-        return user_phone;
-    }
-
-    public void setUser_phone(String user_phone) {
-        this.user_phone = user_phone;
-    }
-
-    public String getUser_addr() {
-        return user_addr;
-    }
-
-    public void setUser_addr(String user_addr) {
-        this.user_addr = user_addr;
-    }
 }
